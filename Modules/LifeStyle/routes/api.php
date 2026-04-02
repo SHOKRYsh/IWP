b5bcdc3c6f8c9a711b3b15ee224f5a737d0e1ea1
SHOKRYsh <shokrymansor123@Gmail.com>
2026-04-02 17:28:27 +0200

    overwrite of routes

--- Modules/LifeStyle/routes/api.php
+++ Modules/LifeStyle/routes/api.php
@@ -5,10 +5,10 @@ use Modules\LifeStyle\Http\Controllers\LifeStyleController;
 use Modules\LifeStyle\Http\Controllers\LifeElementController;
 
 Route::get('/life-style', [LifeStyleController::class, 'index']);
-Route::get('/life-style/{id}', [LifeStyleController::class, 'show']);
+Route::get('/life-style/{id}', [LifeStyleController::class, 'show'])->where('id', '[0-9]+');
 
 Route::get('/life-style/elements', [LifeElementController::class, 'index']);
-Route::get('/life-style/elements/{id}', [LifeElementController::class,'show']);
+Route::get('/life-style/elements/{id}', [LifeElementController::class,'show'])->where('id', '[0-9]+');
 
 Route::middleware(['auth:sanctum'])->prefix('life-style')->group(function () {
     // LifeStyle Routes
